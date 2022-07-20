ece5c4a039a68ff780b249f337340d1502b62406
Hung-web <hungmanh201102@gmail.com>
2022-07-20 09:47:02 +0700

    Xây dựng UI phần Header #2

---
+++ src/components/Layout/AccountItem/index.js
@@ -0,0 +1,23 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+  return (
+    <div className={cx('wrapper')}>
+      <img className={cx('avatar')} src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/094cbc21cbf705c4e5518f478c7deaf1~c5_100x100.jpeg?x-expires=1658397600&x-signature=DZEp0UegGAr865Ntv3sqXROYtNo%3D" alt="Hoaa" />
+      <div className={cx('info')}>
+        <p className={cx('name')}>
+          <h4>Nguyen Manh Hung</h4>
+          <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+        </p>
+        <span className={cx('username')}>Hung</span>
+      </div>
+    </div>
+  );
+}
+
+export default AccountItem;
